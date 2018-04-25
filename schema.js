e507ef61e87b82d132c881bf5136d5b6ec2f05f8
Alexander Ivanov <aivanov@informatica.ru>
2018-04-25 13:46:48 +0300

    init

---
+++ schema.js
@@ -0,0 +1,59 @@
+const pubsub = require('./pubsub');
+const { makeExecutableSchema } = require('graphql-tools');
+const data = [
+  { id: '1', name: 'Petya' },
+  { id: '2', name: 'Vasya' },
+  { id: '3', name: 'Tolik' },
+];
+
+// SCHEMA DEFINITION
+const typeDefs = `
+type Query {
+  users: [User]
+  user(id: ID!): User
+}
+type Subscription {
+  userChanged: User!
+}
+type Mutation {
+  change(id: ID!, name: String!): User
+}
+type User {
+  id: ID!
+  name: String
+}`;
+
+// RESOLVERS
+const resolvers = {
+  Query: {
+    users: (root, args, context, info) => data,
+    user: (root, args, context, info) => {
+      return data.find(item => item.id == args.id);
+    },
+  },
+  Subscription: {
+    userChanged: {
+      subscribe: () => {
+        return pubsub.asyncIterator('USER_CHANGED');
+      },
+    },
+  },
+  Mutation: {
+    change: (parent, { id, name }) => {
+      const user = data.find(item => item.id == id);
+      if (user) {
+        user.name = name;
+      }
+      pubsub.publish('USER_CHANGED', {
+        userChanged: user,
+      });
+      return user;
+    },
+  },
+};
+
+// (EXECUTABLE) SCHEMA
+module.exports = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
